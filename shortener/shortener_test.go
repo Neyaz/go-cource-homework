cb9f6baea0985c0996ef6483cd7d2079435fb50e
Nikolay Malinin <ne9zzz@gmail.com>
2019-05-30 22:46:15 +0300

    Change tabs to spaces

--- shortener/shortener_test.go
+++ shortener/shortener_test.go
@@ -1,53 +1,53 @@
 package shortener
 
 import (
-	"testing"
+  "testing"
 )
 
 var testCases = []struct {
-	url       	string
-	shortenUrl string
+  url        string
+  shortenURL string
 }{
-	{
-		url: "https://otus.ru/some_path?q=23&t=d",
-		shortenUrl: "https://otus.ru/f936",
-	},
-	{
-		url: "otus.ru/some_path?q=23&t=d",
-		shortenUrl: "otus.ru/bd57",
-	},
-	{
-		url: "https://otus.ru/",
-		shortenUrl: "https://otus.ru/",
-	},
-	{
-		url: "otus.ru",
-		shortenUrl: "otus.ru",
-	},
+  {
+    url: "https://otus.ru/some_path?q=23&t=d",
+    shortenURL: "https://otus.ru/f936",
+  },
+  {
+    url: "otus.ru/some_path?q=23&t=d",
+    shortenURL: "otus.ru/bd57",
+  },
+  {
+    url: "https://otus.ru/",
+    shortenURL: "https://otus.ru/",
+  },
+  {
+    url: "otus.ru",
+    shortenURL: "otus.ru",
+  },
 }
 
 func TestShorten(t *testing.T) {
-	for _, c := range testCases {
-		actual := Shorten(c.url)
-		if actual != c.shortenUrl {
-			t.Fatalf("FAIL: URL %q, expected %q, got %q", c.url, c.shortenUrl, actual)
-		}
+  for _, c := range testCases {
+    actual := Shorten(c.url)
+    if actual != c.shortenURL {
+      t.Fatalf("FAIL: URL %q, expected %q, got %q", c.url, c.shortenURL, actual)
+    }
 
-		t.Logf("PASS: URL %q", c.url)
-	}
+    t.Logf("PASS: URL %q", c.url)
+  }
 }
 
 func TestResolve(t *testing.T) {
-	for _, c := range testCases {
-		Shorten(c.url)
-	}
+  for _, c := range testCases {
+    Shorten(c.url)
+  }
 
-	for _, c := range testCases {
-		actual := Resolve(c.shortenUrl)
-		if actual != c.url {
-			t.Fatalf("FAIL: URL %q, expected %q, got %q", c.shortenUrl, c.url, actual)
-		}
+  for _, c := range testCases {
+    actual := Resolve(c.shortenURL)
+    if actual != c.url {
+      t.Fatalf("FAIL: URL %q, expected %q, got %q", c.shortenURL, c.url, actual)
+    }
 
-		t.Logf("PASS: URL %q", c.shortenUrl)
-	}
+    t.Logf("PASS: URL %q", c.shortenURL)
+  }
 }
