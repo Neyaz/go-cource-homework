a57ed79237b7f81747c4375085cd84dde2b53e2c
Nikolay Malinin <ne9zzz@gmail.com>
2019-05-30 22:39:52 +0300

    Fix spaces

--- shortener/shortener.go
+++ shortener/shortener.go
@@ -17,31 +17,31 @@ var re = regexp.MustCompile(`(?:https?:\/\/)?(?:[a-zA-Z0-9.-]+?\.(?:[a-zA-Z])+)\
 
 // Shorten takes url and return short url
 func Shorten(rawURL string) string {
-	var hash string;
-	var shortURL string;
-	matchArr := re.FindStringSubmatch(rawURL)
-	path := re.ReplaceAllString(rawURL, "")
-	if path == "" {
-		shortURL = rawURL
-	} else {
-		hash = hasher(rawURL)
-		shortURL = matchArr[0] + hash
-		urlAccociation[hash] = rawURL
-	}
-	return shortURL
+  var hash string;
+  var shortURL string;
+  matchArr := re.FindStringSubmatch(rawURL)
+  path := re.ReplaceAllString(rawURL, "")
+  if path == "" {
+    shortURL = rawURL
+  } else {
+    hash = hasher(rawURL)
+    shortURL = matchArr[0] + hash
+    urlAccociation[hash] = rawURL
+  }
+  return shortURL
 }
 
 // Resolve takes short url and return orignal url
 func Resolve(rawURL string) string {
-	path := re.ReplaceAllString(rawURL, "")
-	if path == "" {
-		return rawURL
-	}
-	return urlAccociation[path]
+  path := re.ReplaceAllString(rawURL, "")
+  if path == "" {
+    return rawURL
+  }
+  return urlAccociation[path]
 }
 
 func hasher(text string) string {
   algorithm := sha1.New()
   algorithm.Write([]byte(text))
-	return hex.EncodeToString(algorithm.Sum(nil))[0:4]
+  return hex.EncodeToString(algorithm.Sum(nil))[0:4]
 }
