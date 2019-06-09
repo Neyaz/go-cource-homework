713dfc78b0591530171887f355b72fec387c9b24
Nikolay Malinin <ne9zzz@gmail.com>
2019-06-09 16:47:26 +0300

    Analizer

---
+++ analizer/analizer_test.go
@@ -0,0 +1,29 @@
+package analizer
+
+import (
+	"reflect"
+	"testing"
+)
+
+var analizeTestCases = []struct {
+	text     string
+	list     WordsList
+}{
+	{
+		text:     "one one one two",
+		list:     []string{"one", "two"},
+	}
+}
+
+func TestAnalize(t *testing.T) {
+	for _, tt := range analizeTestCases {
+		in := WordsList{}
+		got := in.Analize(tt.text)
+		if !reflect.DeepEqual(WordsList(tt.list), got) {
+			t.Fatalf("FAIL: %s -- expected: %v, actual: %v", tt.text, tt.list, got)
+		} else {
+			t.Logf("PASS: %s", tt.text)
+		}
+
+	}
+}
