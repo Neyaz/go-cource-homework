06275094c3933397a7aeb2d5dc1bb970a49b55a4
Nikolay Malinin <ne9zzz@gmail.com>
2019-06-11 22:35:35 +0300

    Fix: indetion

--- analizer/analizer.go
+++ analizer/analizer.go
@@ -1,8 +1,8 @@
 package analizer
 
 import (
-	"strings"
-	"sort"
+  "strings"
+  "sort"
 )
 
 // WordsList contains most frequent words
@@ -10,26 +10,26 @@ type WordsList []string
 
 // Analize recieves text and return most frequent words
 func (wl WordsList) Analize(text string) WordsList {
-	words := strings.Fields(text)
-	counts := make(map[string]int, len(words))
-	freq := make(map[int][]string, len(words))
-	for _, word := range words {
-		counts[word]++
-	}
-	for k, v := range counts {
+  words := strings.Fields(text)
+  counts := make(map[string]int, len(words))
+  freq := make(map[int][]string, len(words))
+  for _, word := range words {
+    counts[word]++
+  }
+  for k, v := range counts {
       freq[v] = append(freq[v], k)
-	}
-	keys := make([]int, 0, len(counts))
-	for k := range freq {
+  }
+  keys := make([]int, 0, len(counts))
+  for k := range freq {
         keys = append(keys, k)
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(keys)))
-	for _, v := range keys {
+  }
+  sort.Sort(sort.Reverse(sort.IntSlice(keys)))
+  for _, v := range keys {
     for _, s := range freq[v] {
-			if len(wl) <= 10 {
-				wl = append(wl, s)
-			}
-		}
-	}
-	return wl
+      if len(wl) <= 10 {
+        wl = append(wl, s)
+      }
+    }
+  }
+  return wl
 }
--- analizer/analizer_test.go
+++ analizer/analizer_test.go
@@ -1,29 +1,29 @@
 package analizer
 
 import (
-	"reflect"
-	"testing"
+  "reflect"
+  "testing"
 )
 
 var analizeTestCases = []struct {
-	text     string
-	list     WordsList
+  text     string
+  list     WordsList
 }{
-	{
-		text:     "one one one two",
-		list:     []string{"one", "two"},
-	},
+  {
+    text:     "one one one two",
+    list:     []string{"one", "two"},
+  },
 }
 
 func TestAnalize(t *testing.T) {
-	for _, tt := range analizeTestCases {
-		in := WordsList{}
-		got := in.Analize(tt.text)
-		if !reflect.DeepEqual(WordsList(tt.list), got) {
-			t.Fatalf("FAIL: %s -- expected: %v, actual: %v", tt.text, tt.list, got)
-		} else {
-			t.Logf("PASS: %s", tt.text)
-		}
+  for _, tt := range analizeTestCases {
+    in := WordsList{}
+    got := in.Analize(tt.text)
+    if !reflect.DeepEqual(WordsList(tt.list), got) {
+      t.Fatalf("FAIL: %s -- expected: %v, actual: %v", tt.text, tt.list, got)
+    } else {
+      t.Logf("PASS: %s", tt.text)
+    }
 
-	}
+  }
 }
